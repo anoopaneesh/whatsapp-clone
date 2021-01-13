5d3047e54d98087e583dd302ec378e0347e6e39e
Anoop P K <anooppk@pop-os.localdomain>
2021-01-13 09:36:26 +0530

    message

---
+++ src/components/Chatbar/Chatbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chatbar.css'
+
+const Chatbar = () => {
+    return (
+        <div className="chatbar">
+            <h1>This is a chatbar</h1>
+        </div>
+    )
+}
+
+export default Chatbar
